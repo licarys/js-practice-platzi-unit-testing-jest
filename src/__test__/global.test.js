c5cc9b315dfb3dca7e7db8b6791739311de4a421
Lilibeth <lilibeth.sequeiraj@gmail.com>
2022-03-16 12:17:33 -0600

    test: adding tests for numbers, booleans and callbacks

--- src/__test__/global.test.js
+++ src/__test__/global.test.js
@@ -1,5 +1,29 @@
 const text = 'Hola Mundo';
+const fruits = ['manzana', 'banano', 'melon'];
 
 test('Debe contener un texto', () => {
   expect(text).toMatch(/Mundo/);
+});
+
+test('Tenemos banana?', () => {
+  expect(fruits).toContain('banano');
+});
+
+test('Mayor que', () => {
+  expect(10).toBeGreaterThan(9);
+});
+
+
+test('Verdadero', ()=> {
+  expect(true).toBeTruthy();
+})
+
+const reverseString = (text, callback) => {
+  callback(text.split('').reverse().join(''));
+}
+
+test('Probar un callback', () => {
+  reverseString('Hola', (text)=>{
+    expect(text).toBe('aloH');
+  })
 });
